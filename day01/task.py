612e7f22e63df2f9c102d1c894141101f056f28d
Cai Luther <cai.luther789@gmail.com>
2025-08-11 14:02:40 -0500

    added task notes and day 1 project

---
+++ day01/task.py
@@ -0,0 +1,9 @@
+# print("Hello " + input("What is your name?") + "!")
+
+# name = input("What is your name?")
+# print(name)
+# print("hello " + name)
+# print(len(name))
+
+username = "dogluvr99"
+print(len(username))
