f3363d0fbcd84a1121dc475d7e57e10b7d9b393c
Cai Luther <cai.luther789@gmail.com>
2025-08-11 15:23:36 -0500

    added files from udemy pycharm course

--- day01/main.py
+++
@@ -1,3 +0,0 @@
-print("Day 01 – Hello!")
-name = input("What's your name? ")
-print(f"Nice to meet you, {name}!")
--- day01/task.py
+++
@@ -1,9 +0,0 @@
-# print("Hello " + input("What is your name?") + "!")
-
-# name = input("What is your name?")
-# print(name)
-# print("hello " + name)
-# print(len(name))
-
-username = "dogluvr99"
-print(len(username))
