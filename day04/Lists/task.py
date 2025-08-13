2ce79ae8f91296496762afc2e000c40a0d350342
Cai Luther <cai.luther789@gmail.com>
2025-08-13 13:08:50 -0500

    finished day 4 lessons and project

--- day04/Lists/task.py
+++ day04/Lists/task.py
@@ -52,3 +52,14 @@ states_of_america = [
 ]
 
 
+print(states_of_america[-1])
+
+states_of_america[1] = 'Pencilvania'
+print(states_of_america)
+
+states_of_america.append("Pen Island")
+print(states_of_america)
+
+states_of_america.extend(["Pen Island", "CaiLand"])
+print(states_of_america)
+
