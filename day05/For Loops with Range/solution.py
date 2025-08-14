75a9d2b9ed9d661607f14702e331ddcfce3253fb
Cai Luther <cai.luther789@gmail.com>
2025-08-14 11:48:23 -0500

    fizzbuzz task

--- day05/For Loops with Range/solution.py
+++ day05/For Loops with Range/solution.py
@@ -1,14 +1,14 @@
-print(range(1, 10))  # Doesn't do anything
+# print(range(1, 10))  # Doesn't do anything  /
 
-for number in range(1, 10):  # Prints 1 to 9
+# for number in range(1, 10):  # Prints 1 to 9  non upper bound inclusive
+#     print(number)
+
+for number in range(1, 11,3 ): # Prints 1 to 10
     print(number)
 
-for number in range(1, 11):  # Prints 1 to 10
-    print(number)
 
-
-# Gauss challenge
-total = 0
-for number in range(1, 101):
-    total += number
-print(total)
+# # Gauss challenge
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
