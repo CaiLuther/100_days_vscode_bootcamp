7c273b3b1d69fff86425267797eb5eb628595bf9
Cai Luther <cai.luther789@gmail.com>
2025-08-19 13:30:16 -0500

    Added step 1

---
+++ day08/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+def greet():
+    print("helloooo")
+    print("have a chill day")
+    print("ily <3")
+
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"hello {name}")
+
+
+greet_with_name("Cai")
